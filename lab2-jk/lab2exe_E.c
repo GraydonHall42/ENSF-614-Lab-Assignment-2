c1b31111441b8c4a1b9f50e2fafd8a62f31175c7
Jared Kraus <jared.kraus3@gmail.com>
2021-09-27 14:33:57 -0600

    jk C D and E

--- lab2-jk/lab2exe_E.c
+++ lab2-jk/lab2exe_E.c
@@ -5,14 +5,25 @@
  * ENSF 614 Lab 2 Exercise E
  */
 
-#include "lab2exe_D.h"
+#include "lab2exe_E.h"
 
-struct cplx
-cplx_add(struct cplx z1, struct cplx z2)
+struct cplx cplx_add(struct cplx z1, struct cplx z2)
 {
   struct cplx result;
 
   result.real = z1.real + z2.real;
   result.imag = z1.imag + z2.imag;
   return result;
+}
+
+void cplx_subtract(struct cplx z1, struct cplx z2, struct cplx *difference)
+{
+  difference->real = (z1.real - z2.real);
+  difference->imag = (z1.imag - z2.imag);
+}
+
+void cplx_multiply(const struct cplx *pz1, const struct cplx *pz2, struct cplx *product)
+{
+  product->real = (pz1->real * pz2->real) - (pz1->imag * pz2->imag);
+  product->imag = (pz1->real * pz2->imag) + (pz1->imag * pz2->real);
 }
